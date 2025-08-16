4b21bdeb00e9bc32b25eb398a36c894b328a709a
Mikhail Pavlov <i.spiori@gmail.com>
2025-08-16 09:22:41 +0500

    Удален force unwrap, правки дизайна кнопки войти

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -5,5 +5,5 @@ enum Constants {
     static let secretKey = "5usX0JRggRgZU_Yzy3V2lBSNHaCjGNWj-9kjnte4UIE"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
-    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")
 }
